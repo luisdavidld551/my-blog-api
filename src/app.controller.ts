433bfcf21cb99736cac616cd8b37dbe07e5f8321
ldominguez <51376078+luisdavidld551@users.noreply.github.com>
2025-09-23 21:09:30 -0500

    Modificación
    Fix: ajuste de .env

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,17 +1,22 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 import { UsersService } from './users/users.service';
+import { ConfigService } from '@nestjs/config';
+import { Env } from './env.model';
 
 @Controller()
 export class AppController {
   constructor(
     private readonly appService: AppService,
     private readonly service: UsersService,
+    private readonly configService: ConfigService<Env>,
   ) {}
 
   @Get()
   getHello(): string {
-    return this.appService.getHello();
+    const myVar = this.configService.get('MY_VAR', { infer: true });
+    const message = this.appService.getHello();
+    return `${message} ${myVar}`;
   }
 
   @Get('usersApi')
