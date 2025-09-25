d84217cc172946fba281894e4bcbbbfe17c68225
ldominguez <51376078+luisdavidld551@users.noreply.github.com>
2025-09-24 21:55:18 -0500

    Modificación

---
+++ src/users/entitites/profile.entity.ts
@@ -0,0 +1,22 @@
+import { Entity, PrimaryGeneratedColumn, Column, CreateDateColumn, UpdateDateColumn } from 'typeorm';
+
+@Entity({ name: 'profiles' })
+export class Profile {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ type: 'varchar', length: 255 })
+  name: string;
+
+  @Column({ type: 'varchar', length: 255, name: 'last_name' })
+  lastName: string;
+
+  @Column({ type: 'varchar', length: 255 })
+  avatar: string;
+
+  @CreateDateColumn({ type: 'timestamptz', default: () => 'CURRENT_TIMESTAMP', name: 'created_at' })
+  createdAt: Date;
+
+  @UpdateDateColumn({ type: 'timestamptz', default: () => 'CURRENT_TIMESTAMP', name: 'updated_at' })
+  updatedAt: Date;
+}
